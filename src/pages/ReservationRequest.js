37ffe94afd5477e4933202c9bdd0c8bf087ed540
IsmaDavidAA <idaasannmvgd@gmail.com>
2022-04-13 16:46:16 -0400

    T7_Add react router

---
+++ src/pages/ReservationRequest.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ReservationRequest() {
+  return (
+    <div>
+      <h1>Reservation Request</h1>
+      <p>
+        This is the Reservation Request page. It is rendered when the user
+        navigates to /reservation-request.
+      </p>
+    </div>
+  );
+}
