9cb9c0fae76702a0ca260fad856714175d6acc53
IsmaDavidAA <idaasannmvgd@gmail.com>
2022-04-22 16:55:31 -0400

    Tn_add_login colours

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Link,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LogoFCyT from "../../assets/fcyt.png";
+import LogoUMSS from "../../assets/umss.png";
+import LogoKusaSoft from "../../assets/kusasoft.png";
+function Footer() {
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          flexDirection: "column",
+          fontSize: "0.8rem",
+          color: "textSecondary",
+          position: "absolute",
+          bottom: 1,
+          width: "100%",
+          backgroundColor: "#DFE1E6",
+        }}
+      >
+        <Divider
+          sx={{
+            height: 1,
+            backgroundColor: "divider.primary",
+            margin: "10px 0",
+          }}
+        />
+        <Stack
+          sx={{
+            mb: 1,
+          }}
+          direction="row"
+          spacing={1}
+        >
+          <Box
+            component="img"
+            sx={{
+              width: 60,
+              maxWidth: { xs: 60, md: 60 },
+            }}
+            alt="logo fcyt."
+            src={LogoFCyT}
+          />
+          <Box
+            component="img"
+            sx={{
+              minWidth: 120,
+              minHeight: 50,
+              maxWidth: { xs: 120, ys: 50 },
+            }}
+            alt="logo fcyt."
+            src={LogoUMSS}
+          />
+          <Box
+            component="img"
+            sx={{
+              width: 60,
+              maxWidth: { xs: 60, md: 60 },
+            }}
+            alt="logo fcyt."
+            src={LogoKusaSoft}
+          />
+        </Stack>
+        <Box>© {new Date().getFullYear()}, Desarrollado por kusaSoft.</Box>
+      </Box>
+    </>
+  );
+}
+
+export default Footer;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,12 +7,17 @@ import {
   Box,
   Button,
   Container,
+  Divider,
   Grid,
   Link,
+  Paper,
   TextField,
   Typography,
 } from "@mui/material";
 import AuthContext from "../contexts/AuthProvider";
+import { Email, Image, Lock, Password } from "@mui/icons-material";
+
+import Footer from "../components/Footer/Footer";
 
 function Login() {
   const location = useLocation();
@@ -51,51 +56,104 @@ function Login() {
           flexGrow: 1,
           minHeight: "100%",
           height: "100vh",
+          backgroundColor: "#172B4D",
         }}
       >
         <Container
           maxWidth="sm"
           sx={{
-            backgroundColor: "white",
+            backgroundColor: "#fafbfc",
             borderRadius: "10px",
             boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+            marginBottom: "150px",
             padding: "20px",
           }}
         >
           <form onSubmit={formik.handleSubmit}>
-            <Box sx={{ my: 9, textAlign: "center" }}>
-              <Typography color="textPrimary" variant="h2">
-                Iniciar Sesión
+            <Box sx={{ mb: 3, textAlign: "center" }}>
+              <Typography color="textPrimary" variant="h4">
+                Sistema de asignación de aulas
               </Typography>
             </Box>
-            <TextField
-              error={Boolean(formik.touched.email && formik.errors.email)}
-              fullWidth
-              helperText={formik.touched.email && formik.errors.email}
-              label="Correo electrónico"
-              margin="normal"
-              name="email"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              type="email"
-              value={formik.values.email}
-              variant="outlined"
-              sx={{ mt: 5 }}
-            />
-            <TextField
-              error={Boolean(formik.touched.password && formik.errors.password)}
-              fullWidth
-              helperText={formik.touched.password && formik.errors.password}
-              label="Contraseña"
-              margin="normal"
-              name="password"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              type="password"
-              value={formik.values.password}
-              variant="outlined"
-              sx={{ mt: 5 }}
-            />
+
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                mt: 1,
+              }}
+            >
+              <Email
+                sx={{
+                  color: "action.active",
+                  fontSize: "34px",
+                  mr: 1,
+                  my: 0.5,
+                  mb: 1,
+                  alignSelf:
+                    formik.touched.email && formik.errors.email
+                      ? "center"
+                      : "flex-end",
+                }}
+              />
+              <TextField
+                variant="filled"
+                sx={{ minWidth: "fit-content", maxWidth: "300px" }}
+                error={Boolean(formik.touched.email && formik.errors.email)}
+                fullWidth
+                helperText={formik.touched.email && formik.errors.email}
+                label="Correo electrónico"
+                margin="normal"
+                name="email"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="email"
+                value={formik.values.email}
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                mt: 1,
+              }}
+            >
+              <Lock
+                sx={{
+                  color: "action.active",
+                  size: "60px",
+                  mr: 1,
+                  my: 0.5,
+                  mb: 1,
+                  fontSize: "34px",
+                  alignSelf:
+                    formik.touched.password && formik.errors.password
+                      ? "center"
+                      : "flex-end",
+                }}
+              />
+              <TextField
+                error={Boolean(
+                  formik.touched.password && formik.errors.password
+                )}
+                variant="filled"
+                fullWidth
+                sx={{
+                  minWidth: "fit-content",
+                  maxWidth: "300px",
+                }}
+                helperText={formik.touched.password && formik.errors.password}
+                label="Contraseña"
+                margin="normal"
+                name="password"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="password"
+                value={formik.values.password}
+              />
+            </Box>
             <Button
               color="primary"
               disabled={!formik.isValid || formik.isSubmitting}
@@ -110,6 +168,7 @@ function Login() {
           </form>
         </Container>
       </Box>
+      <Footer />
     </>
   ) : (
     <Navigate to="/home" state={{ from: location }} replace />
